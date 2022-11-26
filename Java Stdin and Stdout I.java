c3da4c09df80e0c8da57b0b9436728a5b29f90b3
Logesh <114903346+LOGESH632318@users.noreply.github.com>
2022-11-26 11:41:53 +0530

    Create  Java Stdin and Stdout I.java

---
+++ Java Stdin and Stdout I.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //String s=scan.next();
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        // Complete this line
+        // Complete this line
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        //System.out.println("mystring is:" + s);
+        //System.out.println("myInt is:" + a);
+        // Complete this line
+        // Complete this line
+    }
+}
